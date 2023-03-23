bf731a56446f448b771c55a7b358d2d17291ebc4
Jinal Tandel <jinal.tandel@1rivet.local>
2023-03-23 19:36:12 +0530

    add new project-Learning Resources

---
+++ Practice/project-components/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <learning-resource-list
+      v-for="item in resources"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :link="item.link"
+    ></learning-resource-list>
+  </ul>
+</template>
+<script>
+import LearningResourceList from "./LearningResourceList.vue";
+export default {
+  components: {
+    LearningResourceList,
+  },
+  props: ["resources"],
+};
+</script>
