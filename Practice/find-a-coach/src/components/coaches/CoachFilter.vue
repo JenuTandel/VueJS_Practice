874d4b72746ff0346448e33abbe9f6370ebbba1a
Jinal Tandel <jinal.tandel@1rivet.local>
2023-04-13 19:56:55 +0530

    filter coach

---
+++ Practice/find-a-coach/src/components/coaches/CoachFilter.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <base-card>
+      <h3>Find your coach!!!</h3>
+      <span class="me-2">
+        <input type="checkbox" id="frontend" checked @change="setFilters" />
+        <label for="frontend">FrontEnd</label>
+      </span>
+      <span class="me-2">
+        <input type="checkbox" id="backend" checked @change="setFilters" />
+        <label for="backend">BackEnd</label>
+      </span>
+      <span class="me-2">
+        <input type="checkbox" id="database" checked @change="setFilters" />
+        <label for="database">Database</label>
+      </span>
+      <span class="me-2">
+        <input type="checkbox" id="security" checked @change="setFilters" />
+        <label for="security">Security</label>
+      </span>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CoachFilter",
+  emit: ["change-filter"],
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        database: true,
+        security: true,
+      },
+    };
+  },
+  methods: {
+    setFilters(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updateFilters = { ...this.filters, [inputId]: isActive };
+      this.filters = updateFilters;
+      this.$emit("change-filter", updateFilters);
+    },
+  },
+};
+</script>
--- Practice/find-a-coach/src/components/coaches/CoachesList.vue
+++ Practice/find-a-coach/src/components/coaches/CoachesList.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="p-3 h-100 d-flex flex-column align-items-center">
+    <base-card>
+      <coach-filter @change-filter="setFilter"></coach-filter>
+    </base-card>
     <div class="d-flex">
       <base-button class="outline me-2">Refresh</base-button>
       <base-button link to="/register">Register</base-button>
@@ -20,18 +23,50 @@
 </template>
 <script>
 import CoachItems from "./CoachItems.vue";
+import CoachFilter from "./CoachFilter.vue";
 export default {
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        database: true,
+        security: true,
+      },
+    };
+  },
   components: {
     CoachItems,
+    CoachFilter,
   },
   computed: {
     //get coaches
     filteredCoaches() {
-      return this.$store.getters["coaches/coaches"];
+      const coaches = this.$store.getters["coaches/coaches"];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes("frontend")) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes("backend")) {
+          return true;
+        }
+        if (this.activeFilters.database && coach.areas.includes("database")) {
+          return true;
+        }
+        if (this.activeFilters.security && coach.areas.includes("security")) {
+          return true;
+        }
+        return false;
+      });
     },
     //data is found or not
     hasCoaches() {
       return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+  methods: {
+    setFilter(updateFilters) {
+      this.activeFilters = updateFilters;
     },
   },
 };
