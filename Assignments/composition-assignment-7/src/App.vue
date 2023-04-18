22fcaa1de2bbd970921090287628f18cbb586796
Jinal Tandel <jinal.tandel@1rivet.local>
2023-04-18 20:13:59 +0530

    composition API

---
+++ Assignments/composition-assignment-7/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <!-- <h3 v-if="isVisible">{{ goal }}</h3> -->
+  <!-- <h3 v-if="isVisible">{{ goal }}</h3> -->
+  <h3 v-if="courseData.isVisible">{{ courseData.goal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <!-- <button @click="show">Toggle Goal</button> -->
+  <button @click="show">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<!-- <script>
+//Task 1,2 with 4
+export default {
+  data() {
+    return {
+      goal: "Learn Vue.js",
+      isVisible: true,
+    };
+  },
+  methods: {
+    show() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
+</script> -->
+<!-- <script setup>
+import { ref } from "vue";
+const goal = "Learn Vue.js";
+const isVisible = ref(true);
+
+function show() {
+  isVisible.value = !isVisible.value;
+}
+</script> -->
+<script setup>
+import { reactive } from "vue";
+// const goal = ref("Learn Vue.js");
+// const isVisible = ref(true);
+
+// const courseData = ref({
+//   goal: "Learn Vue.js",
+//   isVisible: true,
+// });
+
+const courseData = reactive({
+  goal: "Learn Vue.js",
+  isVisible: true,
+});
+
+function show() {
+  // isVisible.value = !isVisible.value;
+  // courseData.value.isVisible = !courseData.value.isVisible;
+  courseData.isVisible = !courseData.isVisible;
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
---
+++ Practice/vue-composition-01/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <h2>{{ uname }}</h2>
+  <h2>{{ user.age }}</h2>
+  <h3>
+    {{ user.firstname
+    }}<span>
+      {{ user.lastname }}
+    </span>
+  </h3>
+  <!-- <input type="text" placeholder="Enter your Firstname" @input="setFirstname" /> -->
+  <input
+    type="text"
+    placeholder="Enter your Firstname"
+    v-model="user.firstname"
+  />
+  <h3>{{ number }}</h3>
+  <input type="text" placeholder="Enter your Lastname" @input="setLastname" />
+  <button type="button" @click="setAge">Add Age</button>
+  <button type="button" @click="setNumber">Add</button>
+</template>
+
+<script setup>
+import { reactive, ref, computed, watch } from "vue";
+// const uname = ref("Jinal");
+const number = ref(1);
+const user = reactive({
+  firstname: "Jinal",
+  lastname: "Tandel",
+  age: 26,
+});
+function setAge() {
+  user.age = 27;
+}
+function setNumber() {
+  number.value++;
+}
+setTimeout(() => {
+  uname.value = "Jenu";
+  user.firstname = "Jenu";
+  user.lastname = "Sweet";
+}, 3000);
+
+const uname = computed(function () {
+  return user.firstname + " " + user.lastname;
+});
+watch([number, uname], function (newVal, oldVal) {
+  console.log(newVal);
+  console.log(oldVal);
+});
+// function setFirstname(event) {
+//   user.firstname = event.target.value;
+// }
+function setLastname(event) {
+  user.lastname = event.target.value;
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
